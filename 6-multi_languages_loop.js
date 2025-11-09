4f689cae2ea1cecf7b06a7d8fc53eaa335f0714a
waweru <alvinwaweru43@gmail.com>
2025-11-09 21:06:08 +0300

    Add 6-multi_languages_loop.js to print multiple programming language messages

---
+++ 6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+// Create an array with the 3 lines
+const lines = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// Loop through the array and print each line
+for (const line of lines) {
+    console.log(line);
+}
